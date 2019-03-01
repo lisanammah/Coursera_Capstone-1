19f454d4f617ac19b19856abcbec36ac8e7a1d4e
stolpa4 <stolpa413@gmail.com>
2019-03-01 02:52:49 -0800

    Added several functions. Notebook still in progress.

--- Week3Assignment/utils.py
+++ Week3Assignment/utils.py
@@ -5,5 +5,16 @@ def filter_not_assigned_postcodes(canada_post_codes):
     :return: pd.DataFrame object without garbage
     """
     assigned_postcodes = canada_post_codes['Borough'] != 'Not assigned'
-    return canada_post_codes[assigned_postcodes]
+    return canada_post_codes[assigned_postcodes].reset_index(drop=True)
 
+def name_not_assigned_neighborhoods(canada_post_codes):
+    """
+    In the DataFrame there can be boroughs with not
+    assigned neighborhoods. In this case, we should set the name of
+    the neighborhood same as one in the corresponding borough
+    """
+    not_assigned_neighborhoods = canada_post_codes['Neighbourhood'] == 'Not assigned'
+    canada_post_codes.loc[not_assigned_neighborhoods, 'Neighbourhood'] = \
+        canada_post_codes.loc[not_assigned_neighborhoods, 'Borough']
+
+    return canada_post_codes
