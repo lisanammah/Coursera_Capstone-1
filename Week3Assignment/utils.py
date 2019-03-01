5346acaf6000410d9ee06f159d2764bd62fe6f6f
stolpa4 <stolpa413@gmail.com>
2019-03-01 02:24:50 -0800

    Filtering function added.

--- Week3Assignment/utils.py
+++ Week3Assignment/utils.py
@@ -1,2 +1,9 @@
 def filter_not_assigned_postcodes(canada_post_codes):
-    assigned_postcodes = canada_post_codes
+    """
+    This function filters out not assigned postcodes.
+    :param canada_post_codes: pd.DataFrame object with garbage postcodes
+    :return: pd.DataFrame object without garbage
+    """
+    assigned_postcodes = canada_post_codes['Borough'] != 'Not assigned'
+    return canada_post_codes[assigned_postcodes]
+
