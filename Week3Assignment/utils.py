8e610446b92c0f427c105d4741d225fe30170b48
Stolpovsky <AStolpovsky@ad.harman.com>
2019-03-07 14:00:15 +0300

    get_newarby_venues() function defined, neighbourhoods analysis started.

--- Week3Assignment/utils.py
+++ Week3Assignment/utils.py
@@ -1,6 +1,18 @@
+"""
+Miscellaneous utilitiy functions
+"""
+
 import csv
 
 import geocoder
+import requests
+
+
+foursquare_api_data_dict = {
+    'client_id': 'Z2SF1DMASIEYNAJ3Q2BTQOBASMTYDUFBNLPYUNSEH5SQY35G',
+    'client_secret': 'EP4KN2UJTREORTRILUTH0LEIEI55ACBXV5SKO42G2AKKWNDR',
+    'version': '20180605',
+}
 
 
 def filter_not_assigned_postcodes(canada_post_codes):
@@ -45,10 +57,12 @@ def get_latitude_longitude(postcode):
     latitude_longitude = None
     secure_counter = 10 # To avoid infinite loop
     while latitude_longitude is None and secure_counter:
-        provider_response = geocoder.google('{}, Toronto, Ontario'.format(postcode))
+        provider_response = geocoder.google('{}, Toronto, Ontario'
+                                            .format(postcode))
         latitude_longitude = provider_response.latlng
         secure_counter -= 1
-    if latitude_longitude is None: latitude_longitude = (0, 0)
+    if latitude_longitude is None:
+        latitude_longitude = (0, 0)
     return latitude_longitude
 
 
@@ -74,5 +88,42 @@ def get_latitude_longitude_from_csv(postcode):
         return result
 
 
-def get_nearby_venues(neighborhood, lat, lon)
-    pass
+def get_nearby_venues(neighborhood, lat, lon, radius=500, limit=100):
+    """
+    The function uses Foursquare API to obtain coordinates of nearby venues
+
+    :param neighborhood: name of the neighborhood
+    :type neighborhood: str
+    :param lat: neighborhood latitude
+    :type lat: float
+    :param lon: neighborhood longitude
+    :type lon: float
+    :param radius: radius of the search area, defaults to 500
+    :param radius: int, optional
+    :param limit: limit of venues amount, defaults to 100
+    :param limit: int, optional
+    """
+    venues_list = []
+    basic_url = ('https://api.foursquare.com/v2/venues/explore'
+                 '?&client_id={client_id}&client_secret={client_secret}&v={version}'
+                 .format(**foursquare_api_data_dict) )
+
+    parameters = ('&ll={lat},{lon}&radius={radius}&limit={limit}'
+                  .format(lat=lat, lon=lon, radius=radius, limit=limit) )
+
+    url = basic_url + parameters
+
+    results = requests.get(url).json()['response']['groups'][0]['items']
+
+    for venue in results:
+        venues_list.append({
+            'Neighbourhood': neighborhood,
+            'Neighbourhood Latitude': lat,
+            'Neighbourhood Longitude': lon,
+            'Venue': venue['venue']['name'],
+            'Venue Latitude': venue['venue']['location']['lat'],
+            'Venue Longitude': venue['venue']['location']['lng'],
+            'Venue Category': venue['venue']['categories'][0]['name'],
+        })
+
+    return venues_list
---
+++ tests/test_utils.py
@@ -0,0 +1,4 @@
+from pprint import pprint
+from Week3Assignment.utils import get_nearby_venues
+
+pprint(get_nearby_venues('The Beaches', 43.6763574, -79.2930312))
